651e76fbbf3f5cc08ae2fe4b96290efae39b36e0
mercymungai <mercymungai1997@gmail.com>
2016-10-25 17:56:41 +0300

    creating class

---
+++ mania8.rb
@@ -0,0 +1,16 @@
+class Integer
+	def==getschomp;
+	def_to_eng
+	if self == 5
+		english = 'five' 
+	else
+	english = 'forty-two'
+end
+
+english
+    end
+
+# I would better test a couple of numbers...
+puts 5._to_eng
+puts 42.to_eng
+end
