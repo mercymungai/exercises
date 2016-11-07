1e4f742730536668fe76dadbcf6380e7b83aae34
mercymungai <mercymungai1997@gmail.com>
2016-11-07 23:48:22 +0300

    common routes in URL for in sinatra

---
+++ Route.rb
@@ -0,0 +1,19 @@
+
+get '/' do
+"Triggered via GET"
+18 | Chapter 2: Fundamentals
+post '/' do
+"Triggered via POST"
+end
+put '/' do
+"Triggered via PUT"
+end
+delete '/' do
+"Triggered via DELETE"
+end
+patch '/' do
+"Triggered via PATCH"
+end
+options '/' do
+"Triggered via OPTIONS"
+end
