8b4ace477de6d85e9d576ce9f50508e9a8fbbb86
mercymungai <mercymungai1997@gmail.com>
2016-11-11 17:52:49 +0300

    setting a subclasses

---
+++ subclasses.rb
@@ -0,0 +1,13 @@
+
+require 'sinatra'
+set :title, "My Website"
+# configure let's you specify env dependent options
+configure :development, :test do
+enable :admin_access
+end
+if settings.admin_access?
+get('/admin') { 'welcome to the admin area' }
+end
+get '/' do
+"<h1>#{ settings.title }</h1>"
+end
